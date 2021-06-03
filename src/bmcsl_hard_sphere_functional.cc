071203d28c361e2472c8dd613e99aa64400383a7
Michael Howard <mphoward@auburn.edu>
2021-06-03 13:57:12 -0500

    Only compute excess when needed

--- src/bmcsl_hard_sphere_functional.cc
+++ src/bmcsl_hard_sphere_functional.cc
@@ -53,33 +53,49 @@ void BMCSLHardSphereFunctional::compute(std::shared_ptr<State> state)
                 }
             }
 
-        // compute some auxiliary variables for evaluating free energy and chemical potential
-        const double vf = 1.-xi[3];
-        if (vf < 0)
+        // the excess terms will only be nonzero if xi0 > 0 (nonzero density) and any other
+        // xi (e.g., xi3) > 0 (nonzero diameters)
+        bool has_excess = (xi[0] > 0. && xi[3] > 0.);
+        double energy;
+        if (has_excess)
             {
-            // local void fraction unphysical
-            }
-        const double logvf = std::log(vf);
-        const double vf_2 = vf*vf;
-        const double vf_3 = vf_2*vf;
-        const double xi2_2 = xi[2]*xi[2];
-        const double xi2_3 = xi2_2*xi[2];
-        const double xi3_2 = xi[3]*xi[3];
-        const double xi3_3 = xi3_2*xi[3];
-        const double c1 = 3.*xi[2]/vf;
-        const double c2 = 3.*(xi[1]/vf + xi2_2/xi3_2*logvf + xi2_2/(vf_2*xi[3]));
-        const double c3 = (3.*xi[1]*xi[2]/vf_2 - (xi2_3/xi3_2-xi[0])/vf - 2.*xi2_3/xi3_3*logvf
-                           - xi2_3/(xi3_2*vf_2) + 2.*xi2_3/(xi[3]*vf_3));
+            // compute some auxiliary variables for evaluating free energy and chemical potential
+            const double vf = 1.-xi[3];
+            if (vf < 0)
+                {
+                // local void fraction unphysical
+                }
+            const double logvf = std::log(vf);
+            const double vf_2 = vf*vf;
+            const double vf_3 = vf_2*vf;
+            const double xi2_2 = xi[2]*xi[2];
+            const double xi2_3 = xi2_2*xi[2];
+            const double xi3_2 = xi[3]*xi[3];
+            const double xi3_3 = xi3_2*xi[3];
+            const double c1 = 3.*xi[2]/vf;
+            const double c2 = 3.*(xi[1]/vf + xi2_2/xi3_2*logvf + xi2_2/(vf_2*xi[3]));
+            const double c3 = (3.*xi[1]*xi[2]/vf_2 - (xi2_3/xi3_2-xi[0])/vf - 2.*xi2_3/xi3_3*logvf
+                               - xi2_3/(xi3_2*vf_2) + 2.*xi2_3/(xi[3]*vf_3));
 
-        // compute chemical potential
-        for (size_t i=0; i < num_types; ++i)
+            // compute chemical potential
+            for (size_t i=0; i < num_types; ++i)
+                {
+                const auto di = diams[i];
+                derivs[i][idx] = -logvf + di*(c1+di*(c2+di*c3));
+                }
+
+            // compute free energy
+            energy = dx*(6./M_PI)*((xi2_3/xi3_2-xi[0])*logvf + 3.*xi[1]*xi[2]/vf + xi2_3/(xi[3]*vf_2));
+            }
+        else
             {
-            const auto di = diams[i];
-            derivs[i][idx] = -logvf + di*(c1+di*(c2+di*c3));
+            for (size_t i=0; i < num_types; ++i)
+                {
+                derivs[i][idx] = 0.;
+                }
+            energy = 0.;
             }
-
-        // compute free energy
-        value_ += dx*(6./M_PI)*((xi2_3/xi3_2-xi[0])*logvf + 3.*xi[1]*xi[2]/vf + xi2_3/(xi[3]*vf_2));
+        value_ += energy;
         }
     }
 
