de14740637396ac594ef9cd9a2ab53cffde66c17
Mayukh Kundu <mzk0148@auburn.edu>
2022-09-26 11:34:25 -0500

    Changes made to WB and WB II with modifications made to reduce arithmatic operations

--- src/white_bear.cc
+++ src/white_bear.cc
@@ -26,18 +26,20 @@ void WhiteBear::computePrefactorFunctions(double& f1,
     f1 = -logvf;
     f2 = vfinv;
     df1dn3 = vfinv;
-    df2dn3 = vfinv*vfinv;
-    const double n3sq = n3*n3;
-    if (n3<0.01)
-    {
-    f4 = (18+n3*(32+45*n3))/(432*M_PI);
-    df4dn3 = (70+3*n3*(65+126*n3))/(1080*M_PI) ;
-    }
+    const double vfinvsq = vfinv*vfinv;
+    df2dn3 = vfinvsq;
+    const double n3tol = 1.e-2;
+    if (n3 > n3tol)
+        {
+        const double n3sq = n3*n3;
+        f4 = (n3*vfinvsq+logvf)/(36.*M_PI*n3sq);
+        df4dn3 = (-n3*(2.+(-5.*n3+n3sq))*(vfinvsq*vfinv)+2.*logvf)/(36.*M_PI*n3sq*n3);
+        }
     else
-    {
-    f4 = (n3+vf*vf*logvf)*(vfinv*vfinv)*(1/(n3sq*36*M_PI));
-    df4dn3 = -1*(n3*(-2+(5-n3)*vf*n3)+2*vf*vf*vf*logvf)*(vfinv*vfinv*vfinv)*(1/(n3sq*n3*M_PI));
-    }
+        {
+        f4 = (18.+n3*(32.+45.*n3))/(432.*M_PI);
+        df4dn3 = (80.+9.*n3*(25.+48.*n3))/(1080.*M_PI) ;
+        }
     }
 
 }
--- src/white_bear_mark_ii.cc
+++ src/white_bear_mark_ii.cc
@@ -24,21 +24,23 @@ void WhiteBearMarkII::computePrefactorFunctions(double& f1,
     const double logvf = std::log(vf);
     const double vfinv = 1./vf;
     const double n3sq = n3*n3;
+    const double n3tol = 1.e-2;
     f1 = -logvf;
     df1dn3 = vfinv;
-    if (n3<0.01)
-    {
-    f2 = 1+n3+10*n3sq/9;
-    f4 = (18+n3*(28+39*n3))/(432.*M_PI);
-    df2dn3 = 1+(20/9)*n3+(7/2)*n3sq;
-    df4dn3 = (70+3.*n3*(65+126.*n3))/(1080*M_PI); 
-    }
+    if (n3>n3tol)
+        {
+        const double vfinvsq = vfinv*vfinv;
+        f2 = (5.*vfinv-n3sq*vfinv+2.*logvf)/(3.*n3);
+        f4 = -1.*(n3*vfinvsq+(-3.+n3)*n3sq*vfinvsq+logvf)/(n3sq*36.*M_PI);
+        df2dn3 = (-2.*vfinvsq*(n3-3.*n3sq+vf*vf*logvf))/(3.*n3sq);
+        df4dn3 = (n3*(2.*vfinvsq*vfinv+(-5.*n3+n3sq)*vfinvsq)+2.*logvf)/(36.*n3sq*n3*M_PI);
+        }
     else
-    {
-    f2 = (5.*n3-(n3sq)-2.*logvf*vf)*(vfinv/(3.*n3));
-    f4 = -1*(n3+(-3+n3)*n3sq+vf*vf*logvf)*(vfinv*vfinv)*(1/(n3sq*36*M_PI));
-    df2dn3 = (-2.)*(vfinv*vfinv)*(1/(3*n3sq))*(n3-3.*n3sq+vf*vf*logvf);
-    df4dn3 = -1*(n3*(-2+(5.-n3)*vf*n3)-2.*vf*vf*vf*logvf)*(vfinv*vfinv*vfinv)*(1/(36.*n3sq*n3*M_PI));
-    }
+        {
+        f2 = 1.+n3+10.*n3sq/9.;
+        f4 = (18.+n3*28.+39.*n3sq)/(432.*M_PI);
+        df2dn3 = 1.+(20./9.)*n3+(7./2.)*n3sq;
+        df4dn3 = (70.+195.*n3+378.*n3sq)/(1080.*M_PI); 
+        }
     }
 }
