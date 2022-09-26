0bea699e8faa228547bec1945409ce6ba8dded6a
Mayukh Kundu <mzk0148@auburn.edu>
2022-09-26 15:18:02 -0500

    Errors corrected, equations condensed and updates to formatting

--- src/white_bear.cc
+++ src/white_bear.cc
@@ -19,26 +19,30 @@ void WhiteBear::computePrefactorFunctions(double& f1,
     const double vf = 1.-n3;
     if (vf < 0)
         {
-        // local void fraction unphysical
+        // local void frac
         }
     const double logvf = std::log(vf);
     const double vfinv = 1./vf;
+  
+    
     f1 = -logvf;
     f2 = vfinv;
-    df1dn3 = vfinv;
+    df1dn3 = vfinv;    
     const double vfinvsq = vfinv*vfinv;
     df2dn3 = vfinvsq;
     const double n3tol = 1.e-2;
     if (n3 > n3tol)
         {
-        const double n3sq = n3*n3;
-        f4 = (n3*vfinvsq+logvf)/(36.*M_PI*n3sq);
-        df4dn3 = (-n3*(2.+(-5.*n3+n3sq))*(vfinvsq*vfinv)+2.*logvf)/(36.*M_PI*n3sq*n3);
+        const double f4_denom = 36.*M_PI*n3*n3;
+        f4 = (n3*vfinvsq+logvf)/f4_denom;
+        df4dn3 = (-n3*(2.+n3*(-5.+n3))*(vfinvsq*vfinv)+2.*logvf)/(f4_denom*n3);
         }
     else
         {
-        f4 = (18.+n3*(32.+45.*n3))/(432.*M_PI);
-        df4dn3 = (80.+9.*n3*(25.+48.*n3))/(1080.*M_PI) ;
+        // second-order Taylor series to deal with numerics at small n3
+        const double n3sq = n3*n3;
+        f4 = (18.+32.*n3+45.*n3sq)/(432.*M_PI);
+        df4dn3 = (80.+225.*n3+432.*n3sq)/(1080.*M_PI) ;
         }
     }
 
--- src/white_bear_mark_ii.cc
+++ src/white_bear_mark_ii.cc
@@ -23,22 +23,25 @@ void WhiteBearMarkII::computePrefactorFunctions(double& f1,
         }
     const double logvf = std::log(vf);
     const double vfinv = 1./vf;
-    const double n3sq = n3*n3;
-    const double n3tol = 1.e-2;
+   
     f1 = -logvf;
     df1dn3 = vfinv;
+    const double n3tol = 1.e-2;
     if (n3>n3tol)
         {
         const double vfinvsq = vfinv*vfinv;
-        f2 = (5.*vfinv-n3sq*vfinv+2.*logvf)/(3.*n3);
-        f4 = -1.*(n3*vfinvsq+(-3.+n3)*n3sq*vfinvsq+logvf)/(n3sq*36.*M_PI);
-        df2dn3 = (-2.*vfinvsq*(n3-3.*n3sq+vf*vf*logvf))/(3.*n3sq);
-        df4dn3 = (n3*(2.*vfinvsq*vfinv+(-5.*n3+n3sq)*vfinvsq)+2.*logvf)/(36.*n3sq*n3*M_PI);
+        const double f4_denom = 36.*M_PI*n3*n3;
+        f2= ((5.-n3)*vfinv+2.*logvf/n3)/3.;
+        f4 = -(n3*(1.+(-3.+n3)*n3)*vfinvsq+logvf)/f4_denom;
+        df2dn3 = -2.*((1.-3.*n3)*vfinvsq+logvf/n3)/(3.*n3);
+        df4dn3 = (n3*(2.*vfinv+n3*(-5.+n3))*vfinvsq+2.*logvf)/(f4_denom*n3);
         }
     else
-        {
+         {
+        // second-order Taylor series to deal with numerics at small n3
+        const double n3sq = n3*n3;
         f2 = 1.+n3+10.*n3sq/9.;
-        f4 = (18.+n3*28.+39.*n3sq)/(432.*M_PI);
+        f4 = (18.+28.*n3+39.*n3sq)/(432.*M_PI);
         df2dn3 = 1.+(20./9.)*n3+(7./2.)*n3sq;
         df4dn3 = (70.+195.*n3+378.*n3sq)/(1080.*M_PI); 
         }
