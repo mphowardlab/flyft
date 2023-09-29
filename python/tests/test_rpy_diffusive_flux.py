6cfa02cf9de29cb7754aaca6be140ca054890aa2
Mayukh Kundu <mzk0148@auburn.edu>
2023-09-29 16:34:49 -0500

    Remove print statements and format comments

--- python/tests/test_rpy_diffusive_flux.py
+++ python/tests/test_rpy_diffusive_flux.py
@@ -44,31 +44,30 @@ def test_ideal(grand,ig,rpy,state_grand):
     grand.constrain('A', state.mesh.full.L*1.5, grand.Constraint.N)
     rpy.compute(grand,state)
     flux = np.zeros(len(x))
+    """The chemical potential of the ideal gas equation of state given by:            
+    \beta \mu = \ln\lambda \rho(y)  
+    
+    This term is differentiated w.r.t. y and multiplied by \rho(y) to 
+    generate the force produced. This term is then multiplied by the
+    projection of the mobility integrated over the surface of the
+    droplet and integrated with respect to y to generate velocity.
+    This velocity term is then multiplied with the density along the 
+    radial direction x to generate the flux. 
+    
+    In this calculation density field is assumed to be:
+    
+    \rho(x) = 3/10*x
+    
+    Using this the above mentioned procedure, calculation can be done analytically 
+    for a given density field. The calculation of the integration of the mobility 
+    was done in Mathematica and the equations for the two different limits of integration 
+    calculation are implemented below."""
     for i in range(len(x)):
         left_edge = state.mesh.local.lower_bound(i)
         if i==0:
             M = 0
             bd = 0
-        elif i>0 and i<10:
-            """The chemical potential of the ideal gas equation of state given by:
-            
-            \beta \mu = \ln\lambda \rho(y)  
-            
-            This term is differentiated w.r.t. y and multiplied by \rho(y) to 
-            generate the force produced. This term is then multiplied by the
-            projection of the mobility integrated over the surface of the
-            droplet and integrated with respect to y to generate velocity.
-            This velocity term is then multiplied with the density along the 
-            radial direction x to generate the flux. 
-            
-            In this calculation density field is assumed to be:
-            
-            \rho(x) = 3/10*x
-            
-            Using this the above mentioned procedure, calculation can be done analytically for a 
-            given density field. The calculation of the integration of the mobility was done in Mathematica and the 
-            equations for the two different limits of integration calculation are implemented below.
-            """
+        elif i>0 and i<10:            
             #Limits of integration (ai+ak)-x to x+(ai+ak), where ai = ak = 0.5
             bd = 1/(3*np.pi*1.*1.)*3.0/state.mesh.full.L
             M = left_edge*(-5+left_edge**2)/(18)*3.0/state.mesh.full.L
@@ -80,7 +79,6 @@ def test_ideal(grand,ig,rpy,state_grand):
             M = -(1/18)*(5-1/(left_edge**2))*3.0/state.mesh.full.L
         # Multiplying rho(x) into M at the end
         flux[i] = -((3.0/state.mesh.full.L)*left_edge*M+bd)
-        print(left_edge,rpy.fluxes['A'][i],flux[i])
     assert np.allclose(rpy.fluxes['A'][:90], flux[:90],atol = 5e-3)
 
 def test_excess(grand,ig,rpy,state_grand):
@@ -157,16 +155,17 @@ def test_excess(grand,ig,rpy,state_grand):
             bd = -0.031831*(1 + 3*left_edge)
             M =  (1/left_edge)*(-0.005+left_edge*(left_edge*(0.025+left_edge*(0.075))))
         flux[i] = (M+bd)
-        
     assert np.allclose(rpy.fluxes['A'][:90], flux[:90],rtol = 2e-2,atol = 2e-2)
 
 def test_binary_ideal_one(grand,ig,rpy,binary_state_grand):
-    """In this test we are checking if a "two component" system of same size would 
-    generate the same result as the one-compenent system. Same procedure is followed 
-    as shown in the ideal gas equation of state calculation however in this case 
-    two different types are considered with same particle radius ,i.e. ai = ak = 0.5. 
-    The resulting fluxes calculated should match the one-component ideal gas fluxes.
-    The flux equations are computed in Mathematica and implemented below"""
+
+    """In this test we are checking if a "two component" system of same size 
+    would generate the same result as the one-compenent system. Same procedure
+    is followed as shown in the ideal gas equation of state calculation however
+    in this case two different types are considered with same particle radius, 
+    i.e. ai = ak = 0.5. The resulting fluxes calculated should match the 
+    one-component ideal gas fluxes.The flux equations are computed 
+    in Mathematica and implemented below"""
     
     state = binary_state_grand
 
@@ -213,10 +212,8 @@ def test_binary_ideal_one(grand,ig,rpy,binary_state_grand):
             bd2 =  -0.0159155
             #Integrated form of the mobility is equal to \int M dy
             M2 = -0.005/left_edge+0.025*left_edge
-            
         # Multiplying rho(x) into M at the end
         flux[i] = (M1+M2+bd1+bd2)
-        print(left_edge,rpy.fluxes['A'][i],flux[i])
     assert np.allclose(rpy.fluxes['A'][:90], flux[:90],atol = 8e-3)
        
 
@@ -264,8 +261,8 @@ def test_binary_ideal(grand,ig,rpy,binary_state_grand):
     
     """Same procedure followed as shown in the ideal gas equation of state 
     calculation however in this case two different radii of particles are 
-    considered ,i.e. ai = 0.5 and ak = 1.5. The fluxes are computed in Mathematica 
-    and implemented below"""
+    considered ,i.e. ai = 0.5 and ak = 1.5. The fluxes are computed in 
+    Mathematica and implemented below"""
     
     for i in range(len(x)):
         left_edge = state.mesh.local.lower_bound(i)
@@ -292,7 +289,5 @@ def test_binary_ideal(grand,ig,rpy,binary_state_grand):
         else:
             bd2 = -0.0106103
             M2 = -0.076/left_edge+0.095*left_edge
-            
         flux[i] = (M1+M2+bd1+bd2)
-        print(left_edge,rpy.fluxes['A'][i],flux[i])
     assert np.allclose(rpy.fluxes['A'][:85], flux[:85], atol = 3e-2)
