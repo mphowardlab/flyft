9d34de508138fb796df722da7f61fe2156e73e24
Michael Howard <mphoward@auburn.edu>
2023-10-19 21:53:10 -0500

    Fix compile error due to recursion

--- src/mesh.cc
+++ src/mesh.cc
@@ -118,35 +118,6 @@ double Mesh::integrateVolume(int idx, const DataView<const double>& f) const
     return integrateVolume(idx, f(idx));
     }
 
-double Mesh::interpolate(double x, const DataView<double>& f) const
-    {
-    return interpolate(x,f);
-    }
-
-double Mesh::interpolate(double x, const DataView<const double>& f) const
-    {
-    const auto idx = bin(x);
-    const auto x_c = center(idx);
-
-    double x_0, x_1, f_0, f_1;
-    if (x < x_c)
-        {
-        x_0 = center(idx - 1);
-        x_1 = x_c;
-        f_0 = f(idx - 1);
-        f_1 = f(idx);
-        }
-    else
-        {
-        x_0 = x_c;
-        x_1 = center(idx + 1);
-        f_0 = f(idx);
-        f_1 = f(idx + 1);
-        }
-
-    return f_0 + (x - x_0) * (f_1 - f_0) / (x_1 - x_0);
-    }
-
 double Mesh::gradient(int idx, const DataView<double>& f) const
     {
     if ((idx == 0 && lower_bc_ == BoundaryType::reflect) ||
