cf8df039e7e921d669c3332e9194adc275c4d8bb
Michael Howard <mphoward@auburn.edu>
2025-06-26 14:58:04 -0500

    Draft updates to field

--- include/flyft/field.h
+++ include/flyft/field.h
@@ -21,11 +21,15 @@ class GenericField : public TrackedObject
     using ConstantIterator = typename ConstantView::Iterator;
 
     GenericField() = delete;
-    explicit GenericField(int shape) : GenericField(shape, 0) {}
-    GenericField(int shape, int buffer_shape)
-        : data_(nullptr), shape_(0), buffer_shape_(0), layout_(0)
+
+    explicit GenericField(char num_dimensions, const int* shape)
+        : GenericField(num_dimensions, shape, nullptr)
         {
-        reshape(shape, buffer_shape);
+        }
+
+    GenericField(char num_dimensions, const int* shape, const int* buffer_shape)
+        {
+        reshape(num_dimensions, shape, buffer_shape);
         }
 
     // noncopyable / nonmovable
@@ -36,70 +40,99 @@ class GenericField : public TrackedObject
 
     ~GenericField()
         {
-        if (data_)
-            delete[] data_;
+        delete[] data_;
+        delete[] shape_;
+        delete[] buffer_shape_;
+        delete[] full_shape_;
         }
 
-    T& operator()(int idx)
+    T& operator()(const int* multi_index)
         {
-        return view()(idx);
+        return view()(multi_index);
         }
 
-    const T& operator()(int idx) const
+    const T& operator()(const int* multi_index) const
         {
-        return const_view()(idx);
+        return const_view()(multi_index);
         }
 
-    int shape() const
+    const int* shape() const
         {
         return shape_;
         }
 
-    int buffer_shape() const
+    const int* buffer_shape() const
         {
         return buffer_shape_;
         }
 
-    int full_shape() const
+    const int* full_shape() const
         {
-        return shape_ + 2 * buffer_shape_;
+        return layout_.shape();
         }
 
     View view()
         {
         token_.stageAndCommit();
-        return View(data_, layout_, buffer_shape_, full_shape() - buffer_shape_);
+        return View(data_, layout_, buffer_shape_, shape_);
         }
 
     ConstantView const_view() const
         {
-        return ConstantView(static_cast<const T*>(data_),
-                            layout_,
-                            buffer_shape_,
-                            full_shape() - buffer_shape_);
+        return ConstantView(static_cast<const T*>(data_), layout_, buffer_shape_, shape_);
         }
 
     View full_view()
         {
         token_.stageAndCommit();
-        return View(data_, layout_, 0, full_shape());
+        return View(data_, layout_);
         }
 
     ConstantView const_full_view() const
         {
-        return ConstantView(data_, layout_, 0, full_shape());
+        return ConstantView(data_, layout_);
         }
 
-    void reshape(int shape, int buffer_shape)
+    void reshape(char num_dimensions, const int* shape, const int* buffer_shape)
         {
-        if (shape != shape_ || buffer_shape != buffer_shape_)
+        if (num_dimensions != layout_.num_dimensions())
             {
-            DataLayout layout(shape + 2 * buffer_shape);
+            delete[] shape_;
+            delete[] buffer_shape_;
+            delete[] full_shape_;
+
+            shape_ = new int[num_dimensions] {};
+            buffer_shape_ = new int[num_dimensions] {};
+            full_shape_ = new int[num_dimensions] {};
+            }
+
+        bool same_shape = true;
+        bool same_buffer_shape = true;
+        for (char dim = 0; dim < num_dimensions; ++dim)
+            {
+            if (shape[dim] != shape_[dim])
+                {
+                same_shape = false;
+                shape_[dim] = shape[dim];
+                }
+
+            if (buffer_shape[dim] != buffer_shape_[dim])
+                {
+                same_buffer_shape = false;
+                buffer_shape_[dim] = buffer_shape[dim];
+                }
+
+            full_shape_[dim] = shape_[dim] + 2 * buffer_shape_[dim];
+            }
+
+        if (!(same_shape && same_buffer_shape))
+            {
+            DataLayout layout(num_dimensions, full_shape_);
 
             // if data is alloc'd, decide what to do with it
-            if (data_ != nullptr)
+            if (data_)
                 {
-                if (shape == shape_)
+                if (same_shape)
                     {
                     // data shape is the same but buffer changed, copy
                     T* tmp = new T[layout.size()];
@@ -107,7 +140,7 @@ class GenericField : public TrackedObject
 
                     // copy the data from old to new
                     auto view_old = const_view();
-                    DataView<T> view_new(tmp, layout, buffer_shape, buffer_shape + shape);
+                    DataView<T> view_new(tmp, layout, buffer_shape, shape);
                     std::copy(view_old.begin(), view_old.end(), view_new.begin());
                     std::swap(data_, tmp);
                     delete[] tmp;
@@ -126,26 +159,30 @@ class GenericField : public TrackedObject
                 }
 
             // if data is still not alloc'd, make it so
-            if (data_ == nullptr && layout.size() > 0)
+            if (!data_ && layout.size() > 0)
                 {
                 data_ = new T[layout.size()];
                 std::fill(data_, data_ + layout.size(), T(0));
                 }
-            shape_ = shape;
-            buffer_shape_ = buffer_shape;
             layout_ = layout;
             token_.stageAndCommit();
             }
         }
 
-    void setBuffer(int buffer_shape)
+    void setBuffer(const int* buffer_shape)
         {
         reshape(shape_, buffer_shape);
         }
 
-    void requestBuffer(int buffer_shape)
+    void requestBuffer(const int* buffer_shape)
         {
-        if (buffer_shape > buffer_shape_)
+        bool any_increase = false;
+        for (char dim = 0; !any_increase && dim < layout_.num_dimensions(); ++dim)
+            {
+            any_increase |= (buffer_shape[dim] > buffer_shape_[dim]);
+            }
+
+        if (any_increase)
             {
             setBuffer(buffer_shape);
             }
@@ -153,8 +190,9 @@ class GenericField : public TrackedObject
 
     private:
     T* data_;
-    int shape_;
-    int buffer_shape_;
+    int* shape_;
+    int* buffer_shape_;
+    int* full_shape_;
     DataLayout layout_;
     };
 
